0f02614b9fe846f336cae0d9e50766fd945125c9
Michael Lubrano <michael.lubrano@gmail.com>
2017-04-24 21:17:28 -0700

    Basic Soldier Data
    Added Datastore types and basic Datastore. Created a simple page to add
    a solider and show a listing.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import webapp2
 import jinja2
 import os
+import logging
+import models
+import datetime
+
 
 jinja_environment = jinja2.Environment(
-     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 
 class MainPage(webapp2.RequestHandler):
@@ -16,6 +20,34 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(template.render(template_values))
 
 
+class SoldierPage(webapp2.RequestHandler):
+    def get(self):
+        s_query = models.SoldierData.query()
+        soldier_data = s_query.fetch()
+
+        template_values = {
+            'soldiers' : soldier_data,
+
+        }
+
+        template = jinja_environment.get_template('soldier.html')
+        self.response.out.write(template.render(template_values))
+    def post(self):
+
+        #TODO(Shangpo): Need to fix this to ensure each platoon can work.
+        e = models.SoldierData(
+            soldierName=self.request.get('name'),
+            addedDate= datetime.datetime.now().date(),
+            platoon = self.request.get('platoon')
+        )
+        e.put()
+
+        #TODO(Shangpo): I need to fix this so I don't have to do a refresh.
+
+        return self.redirect('/soldier')
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/soldier', SoldierPage),
 ], debug=True)
---
+++ models.py
@@ -0,0 +1,66 @@
+from google.appengine.ext import ndb
+
+
+class SoldierData(ndb.Model):
+    addedDate = ndb.DateProperty(indexed=False)
+    soldierName = ndb.StringProperty(indexed=False)
+    platoon = ndb.StringProperty(indexed=False)
+    rank = ndb.StringProperty(indexed=False)
+    certRifle = ndb.StringProperty(indexed=False)
+    certNCOPD1 = ndb.IntegerProperty(indexed=False)
+    certNCOPD2 = ndb.IntegerProperty(indexed=False)
+    certNCOPD3 = ndb.IntegerProperty(indexed=False)
+    certAirAssult = ndb.IntegerProperty(indexed=False)
+    certSapper = ndb.IntegerProperty(indexed=False)
+    certRanger = ndb.IntegerProperty(indexed=False)
+    certPathfinder = ndb.IntegerProperty(indexed=False)
+    certFO = ndb.IntegerProperty(indexed=False)
+    certFDC = ndb.IntegerProperty(indexed=False)
+    certJFO = ndb.IntegerProperty(indexed=False)
+    certDMR = ndb.IntegerProperty(indexed=False)
+    certRSLC = ndb.IntegerProperty(indexed=False)
+    certRotor = ndb.IntegerProperty(indexed=False)
+    certRecruit = ndb.IntegerProperty(indexed=False)
+    ribPistol = ndb.IntegerProperty(indexed=False)
+    ribStaff = ndb.IntegerProperty(indexed=False)
+    ribCommand = ndb.IntegerProperty(indexed=False)
+    ribAT = ndb.IntegerProperty(indexed=False)
+    ribGround = ndb.IntegerProperty(indexed=False)
+    ribDM = ndb.IntegerProperty(indexed=False)
+    ripSupport = ndb.IntegerProperty(indexed=False)
+    badgeJump = ndb.IntegerProperty(indexed=False)
+    badgeCIB = ndb.IntegerProperty(indexed=False)
+    badgeIB = ndb.IntegerProperty(indexed=False)
+    badgeMedic = ndb.IntegerProperty(indexed=False)
+    badgeExposive = ndb.IntegerProperty(indexed=False)
+    badgeCAB = ndb.IntegerProperty(indexed=False)
+    badgeAirDefense = ndb.IntegerProperty(indexed=False)
+    badgeArmor = ndb.IntegerProperty(indexed=False)
+    badgeTransport = ndb.IntegerProperty(indexed=False)
+    badgeCombatMedic = ndb.IntegerProperty(indexed=False)
+    medArmedForces = ndb.IntegerProperty(indexed=False)
+    medNationalDefense = ndb.IntegerProperty(indexed=False)
+    medDSM = ndb.IntegerProperty(indexed=False)
+    medMOV = ndb.IntegerProperty(indexed=False)
+    medCommendation = ndb.IntegerProperty(indexed=False)
+    medAchievement = ndb.IntegerProperty(indexed=False)
+    medDsync = ndb.IntegerProperty(indexed=False)
+    medConduct = ndb.IntegerProperty(indexed=False)
+    medPH = ndb.IntegerProperty(indexed=False)
+    medVolunteer = ndb.IntegerProperty(indexed=False)
+    medDSC = ndb.IntegerProperty(indexed=False)
+    medMerit = ndb.IntegerProperty(indexed=False)
+    medBronze = ndb.IntegerProperty(indexed=False)
+    medSilver = ndb.IntegerProperty(indexed=False)
+    medAir = ndb.IntegerProperty(indexed=False)
+    medAFCombatAction= ndb.IntegerProperty(indexed=False)
+    medCombatHelo = ndb.IntegerProperty(indexed=False)
+
+
+
+
+
+
+
+
+
